a20563af89c07fba3e2d654699f2a17b82d7433f
wgooch2000@gmail.com <wgooch2000@gmail.com>
2021-06-03 12:31:23 -0400

    Added Libraries

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// #include "libfbxsdk.so"
+#include <iostream>
+// #include <libfbxsdk.so>
+#include <fbxsdk.h>
+#include <liblava/lava.hpp>
+
+using namespace std;
+
+int main(int argc, char *argv[]) { return 0; }
