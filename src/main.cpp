d91f378ad7a76d82bcafb9b7b037acd125ccada3
wgooch2000@gmail.com <wgooch2000@gmail.com>
2021-06-06 21:59:38 -0400

    Factored Mesh Loading into Liblava Friendlier Form

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,15 @@
 
 #define fn auto
 
-fn read_mesh(FbxNode *node)->std::vector<lava::vertex> {
-  std::vector<lava::vertex> verts_output;
+fn read_mesh(FbxNode *node)->lava::mesh_data {
+  lava::mesh_data output;
   FbxMesh *mesh = node->GetMesh();
   FbxSkin *skin = (FbxSkin *)mesh->GetDeformer(0, FbxDeformer::eSkin);
   size_t tri_count = mesh->GetPolygonCount();
   FbxVector4 *ctrl_points = mesh->GetControlPoints();
   for (size_t i = 0; i < tri_count; i++) {
     for (size_t j = 0; j < 3; j++) {
-      verts_output.push_back(lava::vertex{
+      output.vertices.push_back(lava::vertex{
           .position =
               lava::v3{
                   static_cast<float>(
@@ -29,28 +29,28 @@ fn read_mesh(FbxNode *node)->std::vector<lava::vertex> {
                       ctrl_points[mesh->GetPolygonVertex(i, j)][2]),
               },
       });
-      // TODO: Other fields need to be read.
+      // TODO: Other vertex fields need to be read.
     }
   }
-  return verts_output;
+  return output;
 }
 
-void traverse_node(FbxNode *node) {
+template <typename T> fn traverse_node(FbxNode *node)->T {
   FbxNodeAttribute *attribute = node->GetNodeAttribute();
   switch (attribute->GetAttributeType()) {
   case FbxNodeAttribute::eMesh:
-    // TODO: Deserialize mest.
+    return read_mesh(node);
     break;
   default:
     break;
   }
   for (size_t i = 0; i < node->GetChildCount(); i++) {
-    traverse_node(node->GetChild(i));
+    traverse_node<T>(node->GetChild(i));
   }
 }
 
 int main(int argc, char *argv[]) {
-  // TODO: Initialize the path.
+  // Load and read the mesh from an FBX.
   std::string path = "../res/Idle.fbx";
   FbxManager *manager = FbxManager::Create();
   FbxIOSettings *io_settings = FbxIOSettings::Create(manager, IOSROOT);
@@ -62,7 +62,12 @@ int main(int argc, char *argv[]) {
   importer->Import(scene);
   importer->Destroy();
   FbxNode *root_node = scene->GetRootNode();
-  traverse_node(root_node);
+  lava::mesh_data loaded_data = traverse_node<lava::mesh_data>(root_node);
   std::cout << "Path: " << path;
-  return 0;
+  // Render the mesh.
+  lava::app app("DEV 5 - WGooch", {argc, argv});
+  app.setup();
+  lava::mesh::ptr mesh = lava::make_mesh();
+  mesh->add_data(loaded_data);
+  return EXIT_SUCCESS;
 }
