9a579bb0cfd0f0f2939571c7470ddd4f6567a38d
Conscat <wgooch2000@gmail.com>
2021-07-13 16:15:11 -0400

    Made some changes

--- res/frag.glsl
+++ res/frag.glsl
@@ -13,40 +13,44 @@ layout(binding = 6) uniform sampler2D specular_color_map;
 
 layout(location = 0) in vec4 in_col;
 layout(location = 1) in vec2 in_uv;
-layout(location = 2) in vec3 in_pos;
-layout(location = 3) in vec3 in_norm;
+layout(location = 2) in vec4 in_pos_vert;
+layout(location = 3) in vec4 in_pos_view;
+layout(location = 4) in vec4 in_norm;
 
 layout(location = 0) out vec4 out_color;
 
 // Light source
-vec3 light_pos = vec3(10, -2, 20);
+vec3 light_pos = vec3(0.5, -3, 5);
 vec3 ambience = vec3(0.25, 0.25, 0.25);
 
 // Material properties
-float shininess = 30.26;
+float shininess = 0.076;
 
 void main() {
     vec3 diffuse_color = texture(diffuse_color_map, in_uv).rgb;
     vec3 specular_color = texture(specular_color_map, in_uv).rgb;
-    vec3 normal_color = (texture(normal_color_map, in_uv).xyz);
+    vec3 normal_color = texture(normal_color_map, in_uv).xyz;
 
-    vec3 light_dir = light_pos - in_pos;
-    light_dir = normalize(light_dir)*sign(light_dir);
-    float lambertian_intensity = max(dot(light_dir, in_norm), 0.0);
+    vec3 light_dir = // normalize
+        (light_pos.xyz - in_pos_vert.xyz);
+                                    float sq_dist_light = dot(light_dir, light_dir);
+                                    light_dir = light_dir / sq_dist_light;
+    float lambertian_intensity = max(dot(light_dir, normalize(in_norm.xyz)), 0.0);
 
-    // Draw vector from <0,0,0> (the view pos) to the vertex coordinates.
-    vec3 view_dir = normalize(-in_pos);
+    vec3 view_dir = // normalize
+        (in_pos_view.xyz - in_pos_vert.xyz);
+                                    float sq_dist_view = dot(view_dir, view_dir);
+                                    view_dir = view_dir / sq_dist_view;
     vec3 half_dir = normalize(light_dir + view_dir);
-    float specular_intensity = pow(max(dot(normal_color, half_dir), 0),
-                                    shininess);
+    float specular_intensity = pow(max(dot(normalize(in_norm.xyz), half_dir), 0),
+                                   1 + shininess);
+
+    float light_intensity = 10;
 
     vec3 ambient = ambience * diffuse_color;
-    vec3 diffuse = lambertian_intensity * diffuse_color;
-    vec3 specular = specular_intensity * specular_color;
+    vec3 diffuse = lambertian_intensity * diffuse_color * light_intensity;
+    vec3 specular = specular_intensity * specular_color * light_intensity;
     diffuse -= specular;
     diffuse = max(diffuse, 0);
-    out_color = vec4(
-        ambient
-                     + specular
-                     + diffuse, 1);
+    out_color = vec4(ambient + diffuse + specular, 1);
 }
--- res/vert.glsl
+++ res/vert.glsl
@@ -12,14 +12,15 @@ layout(binding = 0) uniform Ubo_Camera {
 ubo_camera;
 
 layout(binding = 1) uniform Ubo_Model {
-    mat4 model;
+    mat4 world;
 }
 ubo_model;
 
 layout(location = 0) out vec4 out_col;
 layout(location = 1) out vec2 out_uv;
-layout(location = 2) out vec3 out_pos;
-layout(location = 3) out vec3 out_norm;
+layout(location = 2) out vec4 out_pos_vert;
+layout(location = 3) out vec4 out_pos_view;
+layout(location = 4) out vec4 out_norm;
 
 out gl_PerVertex {
     vec4 gl_Position;
@@ -28,15 +29,13 @@ out gl_PerVertex {
 void main() {
     vec4 vert_pos_inv = vec4(in_pos.x, in_pos.y * -1, in_pos.z, 1.0);
     gl_Position = ubo_camera.projection  * ubo_camera.view
-        * ubo_model.model * vert_pos_inv;
-    vec4 vert_pos_4 = // ubo_camera.view *
-        ubo_model.model * vert_pos_inv;
-    // vert_pos_4 *= ubo_camera.projection;
-    out_pos = vec3(vert_pos_4);
-    // out_pos.x = -dot(ubo_camera.view[0].xyz, ubo_camera.view[3].xyz);
-    // out_pos.y = -dot(ubo_camera.view[1].xyz, ubo_camera.view[3].xyz);
-    // out_pos.z = -dot(ubo_camera.view[2].xyz, ubo_camera.view[3].xyz);
+        * ubo_model.world * vert_pos_inv;
+    out_pos_vert = ubo_model.world * vert_pos_inv;
+    out_pos_view.x = -dot(ubo_camera.view[3].xyz, ubo_camera.view[0].xyz);
+    out_pos_view.y = dot(ubo_camera.view[3].xyz, ubo_camera.view[1].xyz);
+    out_pos_view.z = -dot(ubo_camera.view[3].xyz, ubo_camera.view[2].xyz);
+    out_pos_view.w = 1;
     out_col = in_col;
     out_uv = in_uv;
-    out_norm = in_norm;
+    out_norm = ubo_model.world * vec4(in_norm, 0);
 }
